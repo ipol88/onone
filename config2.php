de2e562db0e2a0e3f20db0f7eb5063ee2c5dea84
ipol88 <94164952+ipol88@users.noreply.github.com>
2022-08-24 01:11:19 +0700

    Add files via upload

---
+++ config2.php
@@ -0,0 +1,10 @@
+<?php
+
+// untuk menambahkan tinggal copy yang ada terus paste di bawahnya, 
+// ganti kode negara sesuai countrycode.org
+
+$linkOffer = array( 
+    "ALL" => "https://www.detik.com", // Yang ini jangan dihapus, sebagai default
+    "US" => "https://www.sma479.com/scripts/un981c6l?a_aid=c295c36a&a_bid=9a3a0d83",
+    "SG" => "https://www.sma479.com/scripts/un981c6l?a_aid=c295c36a&a_bid=9a3a0d83",
+);
